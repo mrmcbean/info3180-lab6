69e2ec139fd4b020dad079ee8776ec4d0cac4527
Sil <silvaback@Silvers.local>
2021-04-04 00:15:17 -0500

    Completed Lab 6

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,10 +1,110 @@
 /* Add your Application JavaScript */
-const app = Vue.createApp({
+const Home = {
+  name: 'Home',
+  template: `
+    <div class="home">
+      <img src="/static/images/logo.png" alt="VueJS Logo">
+      <h1>{{ welcome }}</h1>
+  </div>
+  `,
   data() {
     return {
-      welcome: 'Hello World! Welcome to VueJS'
+      welcome: 'Welcome to my VueJS App'
     }
   }
+ };
+
+
+
+ const app = Vue.createApp({
+  data() {
+    return {
+      welcome: 'Welcome to VueJS'
+    }
+  },
+  components: {
+    'home': Home,
+    //'news-list': NewsList
+  }
+});
+
+const NewsList = {
+  name: 'NewsList',
+  template: `
+      <div class="news">
+      <br>
+      <h2>News</h2>
+      <br>
+      <ul class="news__list">
+
+      <li v-for="article in articles" class="news__item"> {{ article.title }} <img :src="article.urlToImage"> {{ article.description }} </li>
+     
+
+  
+      
+      </ul>
+      </div>
+
+      <div class="form-inline d-flex justify-content-center">
+        <div class="form-group mx-sm-3 mb-2">
+          <label class="sr-only" for="search">Search</label>
+          <input type="search" name="search" v-model="searchTerm"
+      id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
+      search term here" />
+      <button class="btn btn-primary mb-2" 
+      @click="searchNews"> Search </button>
+        </div>
+      </div>
+  `,
+
+  created() {
+    let self = this;    
+    fetch('https://newsapi.org/v2/top-headlines?country=us',
+{
+    headers: {
+    'Authorization': 'Bearer <your-api-token>'
+    }
+   })
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(data) {
+    console.log(data);
+    self.articles = data.articles;
+    });
+    },
+    data() {
+      return {
+        articles: [],
+        searchTerm: ''
+      }
+    },
+    methods: {
+      searchNews() {
+      let self = this;
+      fetch('https://newsapi.org/v2/everything?q='+
+     self.searchTerm + '&language=en', {
+      headers: {
+      'Authorization': 'Bearer <your-api-token>'
+      }
+     })
+      .then(function(response) {
+      return response.json();
+      })
+      .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+      });
+      }
+      }
+};
+
+const router = VueRouter.createRouter ({
+  history: VueRouter.createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/news', component: NewsList }
+  ]
 });
 
 app.component('app-header', {
@@ -20,10 +120,10 @@ app.component('app-header', {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <router-link to="/" class="nav-link"> Home </router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                  <router-link to="/news" class="nav-link"> News </router-link>
                 </li>
               </ul>
             </div>
@@ -35,12 +135,13 @@ app.component('app-header', {
   }
 });
 
+
 app.component('app-footer', {
   name: 'AppFooter',
   template: `
       <footer>
           <div class="container">
-              <p>Copyright &copy {{ year }} Flask Inc.</p>
+              <p>Copyright &copy {{ year }} McBean Inc. </p>
           </div>
       </footer>
   `,
@@ -51,4 +152,5 @@ app.component('app-footer', {
   }
 })
 
+app.use(router)
 app.mount('#app');
